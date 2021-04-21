58b19364f4cf6172c09005eeba75af781d07bff9
ychen304 <ychen304@hawk.iit.edu>
2021-04-20 20:19:32 -0500

    Delete unnessary images

--- js/script.js
+++ js/script.js
@@ -1,48 +1,29 @@
- function initMap() {
-  alert('Welcom to Las Vegas');
-        
-        //var el = document.getElementById('canvas');
-        // Add the 1st map location
-        var lv = {lat: 36.1699, lng: -115.1398}
-        
-        // Set options for the map location
-        var mapOptions = { 
-          zoom: 10,
-          center: lv,
-          mapTypeControlOptions:{
-          position: google.maps.ControlPosition.BOTTOM_CENTER
-          }
-        };
+  function initMap() {
 
-        // Add a new map 
-        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+  // Add a mao for Las Vegas and set options
+  const lv = {lat: 36.1699, lng: -115.1398}
+  const map = new google.maps.Map(document.getElementById("map"), {
+      zoom: 10,
+      center: lv,
+    });
+   
+   // Add the icon 
+  const beachflag =
+      "https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png";
+    
+  // Add marker
+  const marker = new google.maps.Marker({
+      position: {lat: 36.0955, lng: -115.1761},
+      map: map,
+      icon: beachflag,
+    });
 
+  const infoWindow = new google.maps.InfoWindow({
+  content: '<h1> Luxor Resort & Casino </h1><p> Here is where I lived in my trip. </p>' 
+          });
 
-         const kenan = {
-          url: "images/kenan.png",
-          scaledSize: new google.maps.Size(32, 32)
-          };
-
-
-        // Add marker
-        var luxor = {lat: 36.0955, lng: -115.1761}
-        var marker = new google.maps.Marker({
-        position: luxor,
-        map: map
-        icon: kenan,
-        });
-
-        // Add info for the marker
-        var infoWindow = new google.maps.InfoWindow({
-          content: '<h1> Luxor Resort & Casino </h1><p> Here is where I lived in my trip. </p>' 
-        });
-
-        marker.addListener('click', function(){
-          infoWindow.open(map, marker);
-        });
-}
-
-google.maps.event.addDomListener(window, 'load, init');
-
-
+          marker.addListener('click', function(){
+            infoWindow.open(map, marker);
+          });
+  }
 
